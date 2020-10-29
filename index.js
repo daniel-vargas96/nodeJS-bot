146fe6f005d1ac578172c36299f65b1146abb46c
Daniel Vargas <dannyvargashb@gmail.com>
2020-10-28 17:07:23 -0700

    set up function that will handle bot response and args

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 
+client.on("message", function (message) {
+  if (message.author.bot) return;
+});
+
 client.login(config.BOT_TOKEN);
